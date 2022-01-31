120766335f50ac396f25a7d0586539f86e7aa658
pepijn <68758035+ItsMePepijn@users.noreply.github.com>
2022-01-31 12:56:35 +0100

    woops

--- events/ready.js
+++ events/ready.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'ready',
     once: true,
     execute(client){
-        console.log(`${client.user.displayName} Has logged in!`);
+        console.log(`${client.user.tag} Has logged in!`);
         client.user.setPresence({ activities: [{ name: 'all the lil mushrooms', type: 'WATCHING' }], status: 'dnd' });
     },
 };

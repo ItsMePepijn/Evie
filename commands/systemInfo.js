a21b82d4d491c53e4b6b26ec3508a2680115481f
pepijn <68758035+ItsMePepijn@users.noreply.github.com>
2022-02-13 19:20:02 +0100

    changed command and added inline

--- commands/systemInfo.js
+++ commands/systemInfo.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js'); 
 const db = require('quick.db');
 const os = require('os');
+const ms = require(ms)
 
 module.exports = {
     name:'hosting',
@@ -10,14 +11,12 @@ module.exports = {
         .setColor(db.get('embedColor'))
         .setTitle('Hosting info:')
         .setDescription('I am hosted by your lovely admin, Ikke!\nFor any questions about me or my hosting, contact <@387920955105738753>')
-        .addField('CPU architecture', os.arch())
-        .addField('CPU', os.cpus()[0].model)
-        .addField('Memory', `${Math.round(os.freemem/1000000)} / ${Math.round(os.totalmem/1000000)}`)
-        .addField('Host name', os.hostname())
-        .addField('Load average', `1m: ${os.loadavg()[0]}\n5m: ${os.loadavg()[1]}\n15m: ${os.loadavg()[2]}`)
-        .addField('Platform', os.platform())
-        .addField('Version', os.release())
-        .addField('Uptime', `${os.uptime()}`)
+        .addField('CPU', os.cpus()[0].model, true)
+        .addField('CPU architecture', os.arch(), true)
+        .addField('Memory', `${Math.round(os.freemem/1000000)} / ${Math.round(os.totalmem/1000000)}`, true)
+        .addField('Platform', `${os.platform()} ${os.release()}`, true)
+        .addField('Load average', `1m: ${os.loadavg()[0]}\n5m: ${os.loadavg()[1]}\n15m: ${os.loadavg()[2]}`, true)
+        .addField('Uptime', `${ms(os.uptime()*1000), {long: true}}`, true)
 
         message.channel.send({embeds: [embed]})
     }

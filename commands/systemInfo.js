af63438f7ea77bc6c01741309f0e1e324d7e4ce6
pepijn <68758035+ItsMePepijn@users.noreply.github.com>
2022-02-14 18:04:41 +0100

    whoops

--- commands/systemInfo.js
+++ commands/systemInfo.js
@@ -13,7 +13,7 @@ module.exports = {
         .setDescription('I am hosted by your lovely admin, Ikke!\nFor any questions about me or my hosting, contact <@387920955105738753>')
         .addField('CPU', os.cpus()[0].model, true)
         .addField('CPU architecture', os.arch(), true)
-        .addField('Memory', `${Math.round(os.freemem/1000000)} / ${Math.round(os.totalmem/1000000)}`, true)
+        .addField('Memory', `${Math.round(os.totalmem - os.freemem) / 1000000} / ${Math.round(os.totalmem/1000000)}`, true)
         .addField('Platform', `${os.platform()} ${os.release()}`, true)
         .addField('Load average', `1m: ${os.loadavg()[0]}\n5m: ${os.loadavg()[1]}\n15m: ${os.loadavg()[2]}`, true)
         .addField('Uptime', `${ms(os.uptime()*1000, {long: true})}`, true)

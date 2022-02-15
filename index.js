c29183c698279a31705f17cfb43c4b552fa5900f
pepijn <68758035+ItsMePepijn@users.noreply.github.com>
2022-02-15 17:02:34 +0100

    blank line and color coding

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 //Importing libraries
 const fs = require('fs');
+const chalk = require('chalk');
 const Discord = require('discord.js');
 
 //Client setup
@@ -10,21 +11,26 @@ client.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
-    console.log(`[COMMAND HANDLER] - ${file} has been loaded`);
+	if(command.isDisabled) console.log(chalk.redBright('[COMMAND HANDLER]') + ` - ${file} has been loaded but its disabled!`);
+	else console.log(chalk.green('[COMMAND HANDLER]') + ` - ${file} has been loaded`);
     client.commands.set(command.name, command);
 }
+
+console.log(' ')
 
 //Events setup
 const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
 for (const file of eventFiles) {
 	const event = require(`./events/${file}`);
-    console.log(`[EVENT HANDLER] - ${file} has been loaded`);
+    console.log(chalk.green('[EVENT HANDLER]') + ` - ${file} has been loaded`);
 	if (event.once) {
 		client.once(event.name, (...args) => event.execute(...args));
 	} else {
 		client.on(event.name, (...args) => event.execute(...args));
 	}
 }
+
+console.log(' ')
 
 const {token} = require('./config.json');
 client.login(token);

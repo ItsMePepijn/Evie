c6bd8999a7c4bd75d593c3af2e8bf2743ca7d715
pepijn <68758035+ItsMePepijn@users.noreply.github.com>
2022-02-19 22:46:33 +0100

    resetbalance command! yay

---
+++ commands/resetBalance.js
@@ -0,0 +1,36 @@
+const Discord = require('discord.js');
+const numeral = require('numeral')
+const db = require('quick.db');
+var economy = new db.table('economy');
+
+module.exports = {
+    name: 'resetbalance',
+    description: 'resets the balance of the mentioned user',
+    execute(message, args){
+
+        const embed = new Discord.MessageEmbed();
+        embed.setTimestamp()
+        embed.setColor(db.get('embedColor'))
+
+        if (message.member.permissions.has('ADMINISTRATOR')){
+            if(message.mentions.members.first()){
+                var target = message.mentions.members.first()
+
+                var oldBalance = economy.get(`user_${target.id}.balance`)
+                if(oldBalance === null) {
+                    var oldBalance = 1000
+                }
+
+                economy.set(`user_${target.id}.balance`, 1000)
+
+                if(Number.isInteger(oldBalance)) var oldBalance = numeral(oldBalance).format('0,0')
+                else var oldBalance = numeral(oldBalance).format('0,0.00')
+
+                embed.setTitle(`${target.user.username}'s balance has been reset!`)
+                embed.setDescription(`**Old balance:** ${oldBalance}`)
+
+                message.channel.send({embeds: [embed]})
+            }
+        }
+    }
+}

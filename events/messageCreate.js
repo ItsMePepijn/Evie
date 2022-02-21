c3a3a8b4e22dabef481e949d911be7e8a43b0f41
pepijn <68758035+ItsMePepijn@users.noreply.github.com>
2022-02-21 17:45:15 +0100

    moved verify system to seperate module

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,47 +1,15 @@
 const db = require('quick.db');
 const pfx = db.get('prefix');
-const Discord = require('discord.js')
 const {client} = require('../client')
 var economy = new db.table('economy')
+const {verifiedCheck} = require('../verifySystem')
 
 module.exports = {
     name: 'messageCreate',
     execute(message){
         if(db.get('verifySystem') == true){
             if(message.channel.id == db.get('verifyChannel').id){
-
-                if(message.author.bot || message.member.roles.cache.has(db.get('staffRole').id)) return;
-                const cnt = message.content.trim().split(' ');
-                const memberRole = message.guild.roles.cache.get(db.get('memberRole').id);
-                const verifyLogs = message.guild.channels.cache.get(db.get('verifyLogs').id)
-                const embed = new Discord.MessageEmbed()
-                .setColor(db.get('embedColor'))
-                .setTimestamp()
-
-                function checkAge(cnt){
-                    for(item of cnt){
-                        if(!isNaN(item)){
-                            if(item > 12 && item < 20) return {
-                                check: true,
-                                age: item
-                            }
-                        }  
-                    }
-                }
-
-                const user = checkAge(cnt)
-
-                if(user.check && message.content.toLowerCase().match(db.get('codeword'))){
-                    message.member.roles.add(memberRole);
-                    console.log(`${message.member.user.tag} has veen verified!`)
-
-                    embed.setTitle('Verified new user!')
-                    embed.setDescription(`**User:** <@${message.member.user.id}>\n**Age:** ${user.age}`)
-                    verifyLogs.send({embeds: [embed]})
-                    return message.delete()
-                }else{
-                    return message.delete()
-                }
+                verifiedCheck(message)
             }
         }
         if(message.author.bot || !message.guild) return;
